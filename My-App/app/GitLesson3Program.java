7c9f189f3f7540853e99474ab709c4d89ce0a4f8
Vladyslav Hurevych <gurik1989@ukr.net>
2024-09-21 17:34:17 +0100

    ADD System.out.println("Shop-online for customers")

--- My-App/app/GitLesson3Program.java
+++ My-App/app/GitLesson3Program.java
@@ -3,6 +3,6 @@ package app;
 public class GitLesson3Program {
     public static void main(String[] args) {
         System.out.println("This is shop-online app");
-
+        System.out.println("Shop-online for customers");
     }
 }
